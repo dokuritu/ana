cbb4bf3ccac5d9214c167061c559eaae327c744a
dokuritu <it.clearpro@gmail.com>
2021-06-30 01:49:44 +0900

    second commit

---
+++ android/app/src/main/kotlin/net/yk/ana/ana/MainActivity.kt
@@ -0,0 +1,6 @@
+package net.yk.ana.ana
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:ana/next_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final items = ['kboy1','kboy2','kboy3','4'];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Kboyのflutter大学'),
+      ),
+      body: Container(
+        width: double.infinity,
+        child:ListView(
+
+          children: <Widget>[
+            Container(
+              width: 160.0,
+              height: 80,
+              color: Colors.yellow,
+            ),
+            Container(
+              width: 160.0,
+              height: 80,
+              color: Colors.orange,
+            ),
+            Container(
+              width: 160.0,
+              height: 80,
+              color: Colors.yellow,
+            ),
+            Container(
+              width: 160.0,
+              height: 80,
+              color: Colors.orange,
+            ),
+          ],
+        )
+        ),
+      );
+  }
+}
